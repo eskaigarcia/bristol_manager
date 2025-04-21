3dd3c6d80fa1c4bb06d79d5495ae893ab123b5a3
eskaigarcia <cga24gr030@student.esgerencia.com>
2025-04-21 11:03:21 +0200

    Edit student data v1

--- resources/studentDetailsEdit.js
+++ resources/studentDetailsEdit.js
@@ -5,8 +5,8 @@ const storage = {
 }
 
 function removeDetailsModal() {
-    if (pendingEdits) {
-        alert('No guardaste cambios.')
+    if (storage.pendingEdits) {
+        _ex.ui.toast.make('Guarda o descarta los cambios de las notas rápidas antes de salir.')
     } else {
         document.getElementById('studentDataModal').remove()
     }
@@ -39,15 +39,16 @@ function toggleIBAN2() {
 }
 
 const quickNotes = {
+    // BUG: NEEDS TO BE FIXED
     discard() {
         storage.pendingEdits = false;
-        document.querySelector('#quickNotes textarea').innerText = storage.studentData.alumno.notasRapidas;
+        document.querySelector('#quickNotes textarea').value = storage.studentData.alumno.notasRapidas;
         document.querySelector('#quickNotes div').classList.add('hidden');
     },
 
     save() {
         // CURRENTLY BROKEN
-        let notes = document.querySelector('#quickNotes textarea').innerText;
+        let notes = document.querySelector('#quickNotes textarea').value;
         let id = storage.activeStudent;
         
         fetch('./updateQuickNotes.php', {
@@ -71,3 +72,93 @@ const quickNotes = {
         document.querySelector('#quickNotes div').classList.remove('hidden');
     }
 } 
+
+
+const triggerEdit = {
+    mainDetails() {
+        const display = document.querySelector('#studentDataModal div');
+        display.innerHTML = `<div class="header">
+                <div>
+                    <p>Editando alumno:</p>
+                    <h2>${storage.studentData.alumno.apellidos}, ${storage.studentData.alumno.nombre}</h2>
+                    <div class="spaced-items-sm">${buildChips(storage.studentData.alumno)}</div>
+                </div>
+                <img onclick="removeDetailsModal()" class="iconButton" src="./img/close.png" alt="Cerrar">
+            </div>
+            
+            <div class="body">
+                <div class="editView">
+                    <h3>Modificar datos personales:</h3>
+                    <form>
+                        <table class="camo">
+                            <tr>
+                                <td><label for="nombre">Nombre:</label></td>
+                                <td><input type="text" id="nombre" name="nombre" value="${storage.studentData.alumno.nombre}"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="apellidos">Apellidos:</label></td>
+                                <td><input type="text" id="apellidos" name="apellidos" value="${storage.studentData.alumno.apellidos}"></td>
+                            </tr>
+                            <tr>
+                                <td><label>Mayor de edad:</label></td>
+                                <td class="unlarge">
+                                    <input type="radio" id="adulto_si" name="adulto" value="si" ${(storage.studentData.alumno.esAdulto == 1) ? 'checked' : ''}>
+                                    <label for="adulto_si">Sí</label>
+                                    <input type="radio" id="adulto_no" name="adulto" value="no" ${(storage.studentData.alumno.esAdulto == 0) ? 'checked' : ''}>
+                                    <label for="adulto_no">No</label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><label>Amonestado:</label></td>
+                                <td class="unlarge">
+                                    <input type="radio" id="amonesta_si" name="amonesta" value="si" ${(storage.studentData.alumno.esAmonestado == 1) ? 'checked' : ''}>
+                                    <label for="amonesta_si">Sí</label>
+                                    <input type="radio" id="amonesta_no" name="amonesta" value="no" ${(storage.studentData.alumno.esAmonestado == 0) ? 'checked' : ''}>
+                                    <label for="amonesta_no">No</label>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><label for="dni">DNI:</label></td>
+                                <td><input type="text" id="dni" name="dni" value="${storage.studentData.alumno.dni}"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="telefono">Teléfono:</label></td>
+                                <td><input type="tel" id="telefono" name="telefono" value="${storage.studentData.alumno.telefono}"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="email">Email:</label></td>
+                                <td><input type="email" id="email" name="email" value="${storage.studentData.alumno.email}"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="direccion">Dirección:</label></td>
+                                <td><input type="text" id="direccion" name="direccion" value="${storage.studentData.alumno.direccion}"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="codigo_postal">Código Postal:</label></td>
+                                <td><input type="text" id="codigo_postal" name="codigo_postal" value="${storage.studentData.alumno.cp}"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="localidad">Localidad:</label></td>
+                                <td><input type="text" id="localidad" name="localidad" value="${storage.studentData.alumno.localidad}"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="iban">IBAN:</label></td>
+                                <td><input type="text" id="iban" name="iban" value="${(storage.studentData.alumno.iban != null) ? storage.studentData.alumno.iban : ''}"></td>
+                            </tr>
+                            <tr>
+                                <td><label for="comentarios_medicos">Comentarios Médicos:</label></td>
+                                <td><textarea id="comentarios_medicos" name="comentarios_medicos">${storage.studentData.alumno.comentariosMedicos}</textarea></td>
+                            </tr>
+                            <tr>
+                                <td><label for="notas_rapidas">Notas Rápidas:</label></td>
+                                <td><textarea id="notas_rapidas" name="notas_rapidas">${storage.studentData.alumno.notasRapidas}</textarea></td>
+                            </tr>
+                        </table>
+                        <button>Cancelar</button>
+                        <button type="submit">Enviar</button>
+                    </form>
+                </div>
+            </div>
+            `;
+    }
+}
