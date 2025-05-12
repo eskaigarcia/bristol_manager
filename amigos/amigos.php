a5877c302bc1ee976b81d94f8cd47008d80df979
JoshuaIliffe <jic24gr022@student.esgerencia.com>
2025-05-12 11:59:37 +0200

    amigos configurado

--- amigos/amigos.php
+++ amigos/amigos.php
@@ -8,58 +8,36 @@
         <?php include './components/navView.php'; ?>
         <main>
             <header>
-                <h1>Grupos</h1>
-                <button onclick="createGroup()" class="cta">
-                    <?php include './img/addGroup.svg'; ?>
-                    Nuevo grupo
+                <h1>Amigos</h1>
+                <button onclick="createFriend()" class="cta">
+                    <?php include './img/addFriend.svg'; ?>
+                    Nuevo amigo
                 </button>
             </header>
 
             <div class="card full">
-                <h2>Buscar grupos</h2>
+                <h2>Buscar amigos</h2>
                 <form action='<?php echo $_SERVER["PHP_SELF"]?>' method="get" id="searchBar">
+                    <!-- Filtro de nombre -->
                     <label for="nombre">Nombre:</label>
                     <input type="text" id="nombre" name="nombre">
 
-                    <label for="profesor">Profesor:</label>
-                    <select id="profesor" name="profesor"></select>
+                    <!-- Filtro de tipo de relación -->
+                    <label for="tipo_relacion">Tipo de relación:</label>
+                    <select id="tipo_relacion" name="tipo_relacion">
+                        <option value="">Cualquier tipo</option>
+                        <option value="amigos">Amigos</option>
+                        <option value="familia">Familia</option>
+                        <option value="conocidos">Conocidos</option>
+                    </select>
 
-                    <div class="full subGrid">
-                        <div class="full subGrid">
-                                <label for="precio_min">Precio min:</label>
-                                <input class="mini" type="number" id="precio_min" name="precio_min" step="any" value="0">
-
-                                <label for="precio_max">Precio max:</label>
-                                <input class="mini" type="number" id="precio_max" name="precio_max" step="any" value="200">
-                        </div>
-
-                        <div class="full subGrid">
-                            <label for="horasSemanales">Hrs/semana:</label>
-                            <input type="number" id="horasSemanales" name="horasSemanales" step="0.5" min="0" max="10">
-                            
-                            <label for="curso">Año:</label>
-                            <input type="number" id="curso" name="curso" step="1" min="2020" max="2100">
-                        </div>
-                        
-                    </div>
-                    
-
-                    <div class="full subGrid">
-                        <label for="modalidad">Modalidad:</label>
-                        <select id="modalidad" name="modalidad">
-                            <option value="">Cualquier modalidad</option>
-                            <option value="presencial">Presencial</option>
-                            <option value="online">Online</option>
-                            <option value="hibrido">Híbrido</option>
-                        </select>
-                            
-                        <label for="esIntensivo">Intensivo: </label>
-                        <select id="esIntensivo" name="esIntensivo">
-                            <option value="">Cualquiera</option>
-                            <option value="1">Intensivos</option>
-                            <option value="0">No intensivos</option>
-                        </select>
-                    </div>
+                    <!-- Filtro de estado activo -->
+                    <label for="activo">Estado:</label>
+                    <select id="activo" name="activo">
+                        <option value="">Cualquiera</option>
+                        <option value="1">Activo</option>
+                        <option value="0">Inactivo</option>
+                    </select>
 
                     <div class="full center">
                         <button type="submit" class="cta">
@@ -71,12 +49,12 @@
             </div>
 
             <div class="card full">
-                <?php include './resources/grupos/groupSearch.php' ?>
+                <?php include './resources/amigos/friendSearch.php' ?>
             </div>
 
-            <script src="./resources/grupos/newGroup.js"></script>
-            <script src="./resources/grupos/getTeacherSelector.js"></script>
-            <script src="./resources/grupos/groupDetailsBuild.js"></script>
+            <script src="./resources/amigos/newFriend.js"></script>
+            <script src="./resources/amigos/getFriendSelector.js"></script>
+            <script src="./resources/amigos/friendDetailsBuild.js"></script>
             <script src="./resources/scrollspy.js"></script>
         </main>
     </div>
