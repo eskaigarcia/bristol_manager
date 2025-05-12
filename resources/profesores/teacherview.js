d4d22e876fe22445833e1a9711f3d32908955600
Santiago Hidalgo taoufiki <sht24gr041@student.esgerencia.com>
2025-05-12 12:20:20 +0200

    barra de navegacion

--- resources/profesores/teacherview.js
+++ resources/profesores/teacherview.js
@@ -9,7 +9,7 @@ function mostrarDatosProfe(id) {
     let div = document.createElement('div');
     div.className = 'modal studentData';
     div.id = 'studentDataModal';
-
+//acuerdate luego de cambiar el enlace del href para que la barra de navegacion vaya a todos lados
     div.innerHTML = `
     <div>
         <div class="header">
@@ -19,6 +19,20 @@ function mostrarDatosProfe(id) {
             </div>
             <img onclick="document.body.removeChild(document.getElementById('studentDataModal'))" class="iconButton" src="./img/close.png" alt="Cerrar">
         </div>
+       <div class="tabs-scrollspy">
+    <div class="tab">
+        <a href="#SDVData">
+            <img src="./img/contactInfo.png" alt="Información y datos del maestro">
+            <span>Datos</span>
+        </a>
+    </div>
+    <div class="tab">
+        <a href="#SDVData">
+            <img src="./img/group.png" alt="Información y datos de los alumnos del maestro">
+            <span>Alumnos</span>
+        </a>
+    </div>
+</div>
         <div class="body" id="studentDataView">
             <div class="scrollspySection">
                 <div id="datos">
@@ -26,7 +40,7 @@ function mostrarDatosProfe(id) {
                     <p>Aquí van los datos del profesor.</p>
                 </div>
                 <div id="alumnos">
-                    <h3>Alumnos relacionados de prueba</h3>
+                    <h3>Alumnos relacionados</h3>
                     <p>Lista de alumnos asociados a este profesor.</p>
                 </div>
                 <div id="otros">
